b24778b30dd6cee6a4a0d70a4ec19e6c189b92b4
Mariia Shtefiuk <masha.shtf@gmail.com>
2025-12-07 04:26:53 +0200

    commit

---
+++ src/models/note.js
@@ -0,0 +1,29 @@
+// src/models/note.js
+
+import { Schema, model } from 'mongoose';
+
+const noteSchema = Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    content: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    tag: {
+      type: String,
+      enum: ['Work', 'Personal', 'Meeting', 'Shopping', 'Ideas', 'Travel', 'Finance', 'Health', 'Important', 'Todo'],
+      default: 'Todo',
+    },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  },
+);
+
+export const Note = model('Note', noteSchema);
